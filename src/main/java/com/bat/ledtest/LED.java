1db0cbe765079b13c9c7ec1ce6f0616a908039ef
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-11-26 16:15:07 +0600

    "push"

---
+++ src/main/java/com/bat/ledtest/LED.java
@@ -0,0 +1,14 @@
+package com.bat.ledtest;
+
+public class LED {
+    public static  int OLD_ID = 1;
+    public static  int NEW_ID = 1;
+
+    public static Color color = Color.GREEN;
+
+    public enum Color{
+        DEFAULT,GREEN,RED
+    }
+
+
+}
--- src/main/java/com/bat/ledtest/Main.java
+++ src/main/java/com/bat/ledtest/Main.java
@@ -1,12 +1,68 @@
 package com.bat.ledtest;
 
-import com.bat.splib.SerialPortConnectorV2;
+import com.bat.ledtest.conmanager.CommunicationManager;
+
+import java.util.Scanner;
 
 public class Main {
+   private static Scanner scanner = new Scanner(System.in);
+
+    private static String data;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        SerialPortConnectorV2 in = SerialPortConnectorV2.getInstance();
-        System.out.println(in);
+        System.out.println("com.bat.ledtest.LED TEST");
+        System.out.println("---------------");
+        System.out.println();
+        CommunicationManager led = new CommunicationManager();
+        boolean status = led.connectToSerialPort("COM1");
+
+        if(status){
+            System.out.println();
+            System.out.println("com.bat.ledtest.LED connected..");
+
+        }
+
+        String token = "00000000099#001 R001 ";
+       // 001@@02M202#1 Welcome to Buisness Automation
+        String wc = "003@@01M101#1 Welcome  FRa";
+        String newT = "00000000099#1 Welcome  QPRO Service Test";
+        System.out.println("=============================");
+        int flag = scanner.nextInt();
+
+        switch (flag){
+            case 1:
+                led.writeToPortForReload(newT,true);
+                break;
+            default: led.displayMessage("Welcome to my desk ", 1,1);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        led.writeToPortForReload("00000000001#001 R002 ",true);
+//
+//
+//        try {
+//            Thread.sleep(5_000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+       //led.writeToPortForReload("00000000001# 01 Aditto ",true);
+//
+//        led.disconnectFromSerialPort();
+
+
 
     }
 }
