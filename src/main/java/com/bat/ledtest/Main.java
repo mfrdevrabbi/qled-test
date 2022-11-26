f9d03789b70429548b2c74e078e0e6ac58db2e81
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-11-26 18:04:33 +0600

    config-func -1

--- src/main/java/com/bat/ledtest/Main.java
+++ src/main/java/com/bat/ledtest/Main.java
@@ -21,16 +21,41 @@ public class Main {
 
         }
 
-        String token = "00000000099#001 R001 ";
+      if(true)  {
+            String oldDeviceId = "2";
+            String configDeviceId = "15";
+            String counterNo = "1";
+            if(configDeviceId.length()<2){
+                configDeviceId = "0"+configDeviceId;
+            }
+
+            if(counterNo.length()<2){
+                counterNo = "0"+counterNo;
+            }
+
+            if(oldDeviceId.length()<2){
+                oldDeviceId = "0"+oldDeviceId;
+            }
+
+            String processSetting = "1"+oldDeviceId+"@@"+configDeviceId+"CC"+configDeviceId+"#0"+counterNo;
+             System.out.println(processSetting);
+            led.writeToPortForReload(processSetting,true);
+        }
+
+
+      new Main().systemSleep();
+
+       // 099@@99CC99#099
+        String token = "00000000015#001 1300";
        // 001@@02M202#1 Welcome to Buisness Automation
         String wc = "003@@01M101#1 Welcome  FRa";
-        String newT = "00000000099#1 Welcome  QPRO Service Test";
+        String newT = "0000000001#1 Welcome  QPRO Service Test";
         System.out.println("=============================");
         int flag = scanner.nextInt();
 
         switch (flag){
             case 1:
-                led.writeToPortForReload(newT,true);
+                led.writeToPortForReload(token,true);
                 break;
             default: led.displayMessage("Welcome to my desk ", 1,1);
         }
@@ -64,5 +89,13 @@ public class Main {
 
 
 
+    }
+
+    private void systemSleep(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
