2825e93d4576aa1b9592652ccaa456186f55ce75
FRabbi <mdfazlerabbi.diu@gmail.com>
2022-11-27 17:11:05 +0600

    update

--- src/main/java/com/bat/ledtest/Main.java
+++ src/main/java/com/bat/ledtest/Main.java
@@ -1,72 +1,24 @@
 package com.bat.ledtest;
-
-import com.bat.ledtest.conmanager.CommunicationManager;
+import com.ba.ledlib.connector.LEDCommunicator;
+import com.ba.ledlib.connector.LEDConnector;
+import io.reactivex.disposables.CompositeDisposable;
 
 import java.util.Scanner;
 
 public class Main {
-   private static Scanner scanner = new Scanner(System.in);
+   private static final Scanner scanner = new Scanner(System.in);
 
     private static String data;
     public static void main(String[] args) {
-        System.out.println("com.bat.ledtest.LED TEST");
-        System.out.println("---------------");
-        System.out.println();
-        CommunicationManager led = new CommunicationManager();
-        boolean status = led.connectToSerialPort("COM1");
 
-        if(status){
-            System.out.println();
-            System.out.println("com.bat.ledtest.LED connected..");
-
-        }
-
-      if(true)  {
-            String oldDeviceId = "2";
-            String configDeviceId = "15";
-            String counterNo = "1";
-            if(configDeviceId.length()<2){
-                configDeviceId = "0"+configDeviceId;
-            }
-
-            if(counterNo.length()<2){
-                counterNo = "0"+counterNo;
-            }
-
-            if(oldDeviceId.length()<2){
-                oldDeviceId = "0"+oldDeviceId;
-            }
-
-            String processSetting = "1"+oldDeviceId+"@@"+configDeviceId+"CC"+configDeviceId+"#0"+counterNo;
-             System.out.println(processSetting);
-            led.writeToPortForReload(processSetting,true);
-        }
+        LEDCommunicator communicator = LEDConnector.builder()
+                .portName("COM1")
+                .build();
 
 
-      new Main().systemSleep();
+        communicator.displayMessage(1,"FRabbi");
 
-       // 099@@99CC99#099
-        String token = "00000000015#001 1300";
-       // 001@@02M202#1 Welcome to Buisness Automation
-        String wc = "003@@01M101#1 Welcome  FRa";
-        String newT = "0000000001#1 Welcome  QPRO Service Test";
-        System.out.println("=============================");
-        int flag = scanner.nextInt();
-
-        switch (flag){
-            case 1:
-                led.writeToPortForReload(token,true);
-                break;
-            default: led.displayMessage("Welcome to my desk ", 1,1);
-        }
-
-
-
-
-
-
-
-
+        CompositeDisposable disposable = new CompositeDisposable();
 
 
 
@@ -98,4 +50,67 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
+
+
+//    private void old(){
+//        System.out.println("com.bat.ledtest.LED TEST");
+//        System.out.println("---------------");
+//        System.out.println();
+//        CommunicationManager led = new CommunicationManager();
+//        boolean status = led.connectToSerialPort("COM1");
+//
+//        if(status){
+//            System.out.println();
+//            System.out.println("com.bat.ledtest.LED connected..");
+//
+//        }
+//
+//        if(false)  {
+//            String oldDeviceId = "2";
+//            String configDeviceId = "15";
+//            String counterNo = "1";
+//            if(configDeviceId.length()<2){
+//                configDeviceId = "0"+configDeviceId;
+//            }
+//
+//            if(counterNo.length()<2){
+//                counterNo = "0"+counterNo;
+//            }
+//
+//            if(oldDeviceId.length()<2){
+//                oldDeviceId = "0"+oldDeviceId;
+//            }
+//
+//            String processSetting = "1"+oldDeviceId+"@@"+configDeviceId+"CC"+configDeviceId+"#0"+counterNo;
+//            System.out.println(processSetting);
+//            led.writeToPortForReload(processSetting,true);
+//        }
+//
+//
+//        if(true){
+//
+//            String processColor = "100@@00T102#0      ";
+//            System.out.println("Process Color:: "+processColor);
+//            //  led.writeToPortForReload(processColor,true);
+//        }
+//
+//
+//        new Main().systemSleep();
+//
+//        // 099@@99CC99#099
+//        String token = "00000000002#001 1301";
+//        // 001@@02M202#1 Welcome to Buisness Automation
+//        String wc = "003@@01M101#1 Welcome  FRa";
+//        String newT = "0000000001#1 Welcome  QPRO Service Test";
+//        System.out.println("=============================");
+//        int flag = scanner.nextInt();
+//
+//        switch (flag){
+//            case 1:
+//                led.writeToPortForReload(token,true);
+//                break;
+//            default: led.displayMessage("Welcome to my desk ", 1,1);
+//        }
+//
+//    }
 }
--- src/main/java/com/bat/ledtest/conmanager/CommunicationManager.java
+++ src/main/java/com/bat/ledtest/conmanager/CommunicationManager.java
@@ -1,108 +1,108 @@
-
-package com.bat.ledtest.conmanager;
-
-import com.bat.ledtest.LED;
-import com.bat.splib.SerialPortConnectorV2;
-import io.reactivex.Single;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
-import jssc.SerialPortException;
-
-import java.time.LocalDateTime;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- * @author FRabbi
- */
-public class CommunicationManager {
-    private final SerialPortConnectorV2 serialPort;
-    private boolean serialConnectionSuccessful = false;
-    OnSerialConnectListener listener;
-
-    private final CompositeDisposable disposables = new CompositeDisposable();
-
-    public interface OnSerialConnectListener {
-
-        void onAfterSerialConnect(boolean status);
-    }
-
-    public CommunicationManager() {
-        serialPort = SerialPortConnectorV2.getInstance();
-    }
-
-
-    public boolean connectToSerialPort(String portName) {
-        boolean connectStatus = false;
-        try {
-            serialPort.connect(portName);
-            connectStatus = true;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return connectStatus;
-    }
-
-    public void disconnectFromSerialPort() {
-        System.out.println("-------------disconnect-------------");
-        serialPort.disconnect();
-        serialConnectionSuccessful = false;
-    }
-
-
-    public void sendDataToLed(final String data) {
-        disposables.add(Single
-                .fromCallable(() -> {
-                    serialPort.writetoport(data);
-                    System.out.println(LocalDateTime.now() + ": Sending data to com.bat.ledtest.LED -> " + data + "\n");
-                    return "Write to port done";
-                })
-                .subscribeOn(Schedulers.io())
-                .subscribe(result -> System.out.println("" + result), error -> {
-                    error.printStackTrace();
-                }));
-    }
-
-    public void writeToPortForReload(final String data, boolean isReload) {
-        System.out.println("---------------------------------------");
-        int duration = 0;
-        if (isReload) {
-            duration = 2000;
-        } else {
-            duration = 500;
-        }
-
-        sendDataToLed(data);
-
-        try {
-//                    Thread.sleep(500);
-            if (isReload) {
-                Thread.sleep(1000);
-            } else {
-                Thread.sleep(500);
-            }
-        } catch (Exception ex) {
-          //  java.util.logging.Logger.getLogger(ShowcaseLedUI.class.getName()).log(Level.SEVERE, null, ex);
-            ex.printStackTrace();
-        }
-        System.out.println("---------------------------------------");
-    }
-
-
-
-    public void displayMessage(String message,int oldId, int newId){
-
-        String dataCode;
-        dataCode = "10"+ oldId+"@@0"+newId+"M"+getColor(LED.Color.GREEN)+"02#1 "+message;
-        System.out.printf("AfterDataProcess:: "+dataCode);
-        writeToPortForReload(dataCode,true);
-    }
-
-    private String getColor(LED.Color color){
-        if (color.equals(LED.Color.RED)){
-            return "2";
-        }
-        return "1";
-    }
-
-}
+//
+//package com.bat.ledtest.conmanager;
+//
+//import com.bat.ledtest.LED;
+//import com.bat.splib.SerialPortConnectorV2;
+//import io.reactivex.Single;
+//import io.reactivex.disposables.CompositeDisposable;
+//import io.reactivex.schedulers.Schedulers;
+//import jssc.SerialPortException;
+//
+//import java.time.LocalDateTime;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+///**
+// * @author FRabbi
+// */
+//public class CommunicationManager {
+//    private final SerialPortConnectorV2 serialPort;
+//    private boolean serialConnectionSuccessful = false;
+//    OnSerialConnectListener listener;
+//
+//    private final CompositeDisposable disposables = new CompositeDisposable();
+//
+//    public interface OnSerialConnectListener {
+//
+//        void onAfterSerialConnect(boolean status);
+//    }
+//
+//    public CommunicationManager() {
+//        serialPort = SerialPortConnectorV2.getInstance();
+//    }
+//
+//
+//    public boolean connectToSerialPort(String portName) {
+//        boolean connectStatus = false;
+//        try {
+//            serialPort.connect(portName);
+//            connectStatus = true;
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        return connectStatus;
+//    }
+//
+//    public void disconnectFromSerialPort() {
+//        System.out.println("-------------disconnect-------------");
+//        serialPort.disconnect();
+//        serialConnectionSuccessful = false;
+//    }
+//
+//
+//    public void sendDataToLed(final String data) {
+//        disposables.add(Single
+//                .fromCallable(() -> {
+//                    serialPort.writetoport(data);
+//                    System.out.println(LocalDateTime.now() + ": Sending data to com.bat.ledtest.LED -> " + data + "\n");
+//                    return "Write to port done";
+//                })
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(result -> System.out.println("" + result), error -> {
+//                    error.printStackTrace();
+//                }));
+//    }
+//
+//    public void writeToPortForReload(final String data, boolean isReload) {
+//        System.out.println("---------------------------------------");
+//        int duration = 0;
+//        if (isReload) {
+//            duration = 2000;
+//        } else {
+//            duration = 500;
+//        }
+//
+//        sendDataToLed(data);
+//
+//        try {
+////                    Thread.sleep(500);
+//            if (isReload) {
+//                Thread.sleep(1000);
+//            } else {
+//                Thread.sleep(500);
+//            }
+//        } catch (Exception ex) {
+//          //  java.util.logging.Logger.getLogger(ShowcaseLedUI.class.getName()).log(Level.SEVERE, null, ex);
+//            ex.printStackTrace();
+//        }
+//        System.out.println("---------------------------------------");
+//    }
+//
+//
+//
+//    public void displayMessage(String message,int oldId, int newId){
+//
+//        String dataCode;
+//        dataCode = "10"+ oldId+"@@0"+newId+"M"+getColor(LED.Color.GREEN)+"02#1 "+message;
+//        System.out.printf("AfterDataProcess:: "+dataCode);
+//        writeToPortForReload(dataCode,true);
+//    }
+//
+//    private String getColor(LED.Color color){
+//        if (color.equals(LED.Color.RED)){
+//            return "2";
+//        }
+//        return "1";
+//    }
+//
+//}
